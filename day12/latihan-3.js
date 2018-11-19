c2de289ab3062f4847b4876b2714fc6ddca84a90
thomasfebrianseiei <thomas-f-seiei@hotmail.com>
2018-11-19 18:56:03 +0700

    UPDATE

--- day12/latihan-3.js
+++ day12/latihan-3.js
@@ -1,8 +1,6 @@
 var numbers = [ 10, 8, 1, 9, 1, 7, 29, 1, 0, 4, 1, 7]
 
 function mode(numbers) {
-    if(numbers.length === 0)
-        return null;
     var mode1 = {};
     var maxE = numbers[0]
     var maxCount = 1;
